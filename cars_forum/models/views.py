7a27a01107a92307b3b22d44b89a60a6d03c250d
AlexSki11 <sasha.morozov022@yandex.ru>
2024-11-01 12:22:35 +0300

    add empty views for Car

--- cars_forum/models/views.py
+++ cars_forum/models/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.views.generic import CreateView, UpdateView, DetailView, DeleteView, ListView
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+from .models import Car, Comment, User
 
 # Create your views here.
+
+class CarCreate(LoginRequiredMixin,CreateView):
+    pass
+
+class CarEdit(LoginRequiredMixin, UpdateView):
+    pass
+
+class CarDelete(LoginRequiredMixin, DeleteView):
+    pass
+
+class CarList(ListView):
+    pass
